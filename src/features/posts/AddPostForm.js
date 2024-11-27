10f751b2b1ec16ba6e8749548ae10d5eea55a9a1
ismailtaher <ismail.taher@ymail.com>
2024-11-27 15:59:39 +0500

    First Commit

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewPost } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
+import { useNavigate } from "react-router-dom";
+
+const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
+
+  const users = useSelector(selectAllUsers);
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
+
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
+
+  const onSavePostClicked = () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUserId("");
+        navigate("/");
+      } catch (err) {
+        console.error("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
+    }
+  };
+
+  const userOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          name="postTitle"
+          id="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {userOptions}
+        </select>
+        <label htmlFor="postContent">Post Content:</label>
+        <textarea
+          name="postContent"
+          id="postContent"
+          value={content}
+          onChange={onContentChanged}></textarea>
+      </form>
+      <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+        Save Post
+      </button>
+    </section>
+  );
+};
+
+export default AddPostForm;
